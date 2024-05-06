ee614e7e474f5b48b77f04af5c3e4a3395740623
Ivan112005 <98260798+Ivan112005@users.noreply.github.com>
2024-05-06 13:20:13 +0200

    ultimo

--- ProjectLanchas/Project1/Project1.cpp
+++ ProjectLanchas/Project1/Project1.cpp
@@ -1,71 +1,59 @@
 #include <iostream>
-
-#include "conio.h"
 #include "Enemigo.h"
 
 using namespace std;
 
 int main() {
-    //lanchas
+    // Lanchas
     Lancha lancha1("Lancha 1");
     Lancha lancha2("Lancha 2");
 
-    //juego
+    // Juego
     for (int turno = 1; turno <= 5; turno++) {
-        std::cout << "\nTurno de lancha 1\n ";
-        std::cout << lancha1.lanzarDado();
+        cout << "\nTurno de lancha 1\n ";
+        cout << lancha1.lanzarDado();
         if (lancha1.getNitro()) {
+            cout << "\nPresiona 's' para usar el nitro, o cualquier otra tecla para continuar...\n";
 
-            std::cout << "\nPresiona 's' para usar el nitro, o cualquier otra tecla para continuar...\n";
-
-            char tecla = _getch();
+            char tecla;
+            cin >> tecla;
 
             if (tecla == 's' || tecla == 'S') {
-
                 lancha1.usarNitro();
-
-                std::cout << "Nitro activado!\n";
-
+                cout << "Nitro activado!\n";
             }
-
         }
-
         lancha1.calcularDistancia();
     }
 
-
-    std::cout << "\n------------------------";
+    cout << "\n------------------------";
 
     for (int turno = 1; turno <= 5; turno++) {
-        std::cout << "\nTurno de lancha 2\n ";
-        std::cout << lancha2.lanzarDado();
-       if (lancha2.getNitro()) {
+        cout << "\nTurno de lancha 2\n ";
+        cout << lancha2.lanzarDado();
+        if (lancha2.getNitro()) {
+            cout << "\nPresiona 's' para usar el nitro, o cualquier otra tecla para continuar...\n";
 
-                std::cout << "\nPresiona 's' para usar el nitro, o cualquier otra tecla para continuar...\n";
+            char tecla;
+            cin >> tecla;
 
-                char tecla = _getch();
-
-                if (tecla == 's' || tecla == 'S') {
-
-                    lancha2.usarNitro();
-
-                    std::cout << "Nitro activado!\n";
-
-                }
-
+            if (tecla == 's' || tecla == 'S') {
+                lancha2.usarNitro();
+                cout << "Nitro activado!\n";
             }
+        }
         lancha2.calcularDistancia();
     }
 
-    // el ganador
+    // El ganador
     if (lancha1.getDistanciaRecorrida() > lancha2.getDistanciaRecorrida()) {
-        std::cout << "\nEl ganador es " << lancha1.getNombreCorredor() << "!" << std::endl;
+        cout << "\nEl ganador es " << lancha1.getNombreCorredor() << "!" << endl;
     }
     else if (lancha1.getDistanciaRecorrida() < lancha2.getDistanciaRecorrida()) {
-        std::cout << "\nEl ganador es " << lancha2.getNombreCorredor() << "!" << std::endl;
+        cout << "\nEl ganador es " << lancha2.getNombreCorredor() << "!" << endl;
     }
     else {
-        std::cout << "\nHa habido un empate!" << std::endl;
+        cout << "\nHa habido un empate!" << endl;
     }
 
     return 0;
